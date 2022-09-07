91ec89b5364ee54918bb44b1df58147c6fe040c4
kumar1102 <kumargaurav20182001@gmail.com>
2022-09-08 01:21:16 +0530

    make it better

--- dbConnect.js
+++ dbConnect.js
@@ -1,9 +1,14 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheymoney-udemy' , {useNewUrlParser : true , useUnifiedTopology : true})
+mongoose.connect(
+  "mongodb+srv://sathya:sathyapr@cluster0.wrqpt.mongodb.net/sheymoney-udemy",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.on('error', err => console.log(err))
+connection.on("error", (err) => console.log(err));
 
-connection.on('connected' , () => console.log('Mongo DB Connection Successfull'))
+connection.on("connected", () =>
+  console.log("Mongo DB Connection Successfull")
+);
